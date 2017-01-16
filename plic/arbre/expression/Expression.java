ac685e2e2166cdedea8d83b99aae19990d3e0297
roth52u <roth52u@univ-lorraine.fr>
2017-01-16 17:43:28 +0100

    Compil qui marche à peu près

---
+++ plic/arbre/expression/Expression.java
@@ -0,0 +1,16 @@
+package plic.arbre.expression;
+
+import plic.arbre.ArbreAbstrait;
+
+/**
+ * 3 déc. 2015
+ *
+ * @author brigitte wrobel-dautcourt
+ */
+
+public abstract class Expression extends ArbreAbstrait {
+    
+    protected Expression(int n) {
+        super(n) ;
+    }
+}
