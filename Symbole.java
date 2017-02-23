c20e1cc1e53e5acc71f95a17e4c00feca5f8f9bf
Guillaume ROTH <guillaume.roth7@etu.univ-lorraine.fr>
2017-02-23 14:49:22 +0100

    Tentative de correction d'erreurs générées par Eclipse. Problème au niveau de la fonction next_token de Analyseur Lexical. Il renvoie un Symbol alors que sa déclaration de fonction dit qu'il renvoie un Symbole

--- Symbole.java
+++ Symbole.java
@@ -7,10 +7,21 @@ public class Symbole {
 	int type;
 	int placeDansLaPile;
 	
+	int yyline;
+	int yycolumn;
+	Object value;
+	
 	public Symbole(int p, int typ, int pile){
 		privPub = p;
 		type = typ;
 		placeDansLaPile = pile;
+	}
+
+	public Symbole(int type, int yyline, int yycolumn, Object value) {
+		this.type = type;
+		this.yyline = yyline;
+		this.yycolumn = yycolumn;
+		this.value = value;
 	}
 
 	public int getPrivPub() {
@@ -23,6 +34,18 @@ public class Symbole {
 
 	public int getPlaceDansLaPile() {
 		return placeDansLaPile;
+	}
+	
+	public int getYyline(){
+		return this.yyline;
+	}
+	
+	public int getYycolumn(){
+		return this.yycolumn;
+	}
+	
+	public Object getValue(){
+		return this.value;
 	}
 	
 }
